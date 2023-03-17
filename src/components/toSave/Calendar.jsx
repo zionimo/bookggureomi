35e9d9a5b8b4632525682eea046bc08b9c27e644
zionimo <zionimobu@gmail.com>
2023-03-18 00:09:50 +0900

    install lodash, 독서기간 수정중

--- src/components/toSave/Calendar.jsx
+++ src/components/toSave/Calendar.jsx
@@ -4,22 +4,90 @@ import { useState } from "react";
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { addDays } from "date-fns";
+import { getYear, getMonth, addDays } from "date-fns";
 import { ko } from "date-fns/esm/locale";
 
 const Calendar = () => {
   const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const _ = require("lodash");
+  // range(시작년도, 마지막년도);
+  const years = _.range(2000, getYear(new Date()) + 1, 1);
+  const months = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+  ];
 
   return (
-    <StyleDatePicker
-      showIcon
-      dateFormat="yyyy년 MM월 dd일"
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      maxDate={addDays(new Date(), 5)}
-      startDate={startDate}
-      locale={ko} // 한국어로 변경 props 추가. default는 영어
-    />
+    <>
+      <StyleDatePicker
+        renderCustomHeader={({
+          date,
+          changeYear,
+          changeMonth,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled,
+        }) => (
+          <div
+            style={{
+              margin: 10,
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+              {"<"}
+            </button>
+            <select
+              value={getYear(date)}
+              onChange={({ target: { value } }) => changeYear(value)}
+            >
+              {years.map((option) => (
+                <option key={option} value={option}>
+                  {option}년
+                </option>
+              ))}
+            </select>
+
+            <select
+              value={months[getMonth(date)]}
+              onChange={({ target: { value } }) =>
+                changeMonth(months.indexOf(value))
+              }
+            >
+              {months.map((option) => (
+                <option key={option} value={option}>
+                  {option}월
+                </option>
+              ))}
+            </select>
+
+            <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+              {">"}
+            </button>
+          </div>
+        )}
+        selected={startDate}
+        dateFormat={"yyyy년 MM월 dd일"}
+        locale={ko} // 한국어 적용. default는 영어
+        onChange={(date) => setStartDate(date)}
+        maxDate={addDays(new Date(), 0)}
+        withPortal // 팝업형식
+      />
+    </>
   );
 };
 
